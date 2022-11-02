b5c38c26a7ac77dac2cbf29f9a071a6c870b70cf
alexey.makhonin <alexey.makhonin@flant.com>
2022-11-02 20:35:55 +0300

    Fix webhooks

--- main.go
+++ main.go
@@ -96,14 +96,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "User")
 		os.Exit(1)
 	}
-	// if err = (&authv1alpha1.User{}).SetupWebhookWithManager(mgr); err != nil {
-	// 	setupLog.Error(err, "unable to create webhook", "webhook", "User")
-	// 	os.Exit(1)
-	// }
-	// if err = (&authv1alpha1.Config{}).SetupWebhookWithManager(mgr); err != nil {
-	// 	setupLog.Error(err, "unable to create webhook", "webhook", "Config")
-	// 	os.Exit(1)
-	// }
+	if err = (&authv1alpha1.User{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "User")
+		os.Exit(1)
+	}
+	if err = (&authv1alpha1.Config{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "Config")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
